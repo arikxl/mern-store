7fdc236372cf94879ba431aeba1d492b8b7a62a4
Arik Alexandrov <arikxl@gmail.com>
2023-06-09 16:32:49 +0300

    connect to back

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import data from '../data/data'
+import React, { useEffect, useReducer, useState } from 'react'
+import axios from 'axios';
+
 import ProductPreview from './ProductPreview'
+import { productsReducer } from '../store/reducer';
 
 const ProductList = () => {
+
+    const [{ loading, error, products }, dispatch] = useReducer(productsReducer, {
+        loading: true, error: '', products: []
+    });
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            dispatch({ type: 'FETCH_REQUEST' })
+            try {
+                const result = await axios.get('/api/products');
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+            } catch (error) {
+                dispatch({ type: 'FETCH_FAIL', payload: error.message })
+            }
+        }
+        fetchProducts()
+    }, [])
+
     return (
         <div>
             <h1>ProductList</h1>
             <div className="product-list">
-
                 {
-                    data.products.map(product => (
-                        <ProductPreview product={product} key={product.slug} />
-                    ))
+                    loading ? <div>LOADING...</div>
+                        :
+                        error ? <div>{error}</div>
+                            :
+                            products?.map(product => (
+                                <ProductPreview product={product} key={product.slug} />
+                            ))
                 }
             </div>
         </div>
---
+++ frontend/src/store/reducer.js
@@ -0,0 +1,14 @@
+
+
+export const productsReducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, loading: true };
+        case 'FETCH_SUCCESS':
+            return { ...state, loading: false, products: action.payload };
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload };
+        default:
+            return state
+    }
+}
