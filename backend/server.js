f7fc039841dbb8b4f1e2794889361ec2b64bf0a6
Arik Alexandrov <arikxl@gmail.com>
2023-06-20 02:22:55 +0300

    login api

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 // import data from './data.js';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -15,14 +16,22 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.use('/api/seed', seedRouter)
 
 app.use('/api/products', productRouter)
 // app.get('/api/products', (req, res) => {
-//     res.send(data.products);
-// })
+    //     res.send(data.products);
+    // })
+    
+app.use('/api/users', userRouter);
 
 
+app.use((err, req, res, next) => {
+    res.status(500).send({message: err.message});
+})
 
 const PORT = process.env.PORT || 5000;
 
